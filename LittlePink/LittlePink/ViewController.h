c7ea14ec93bff64516bc2538bc1b52bcf9ddaf0a
qinxinghua <qinxinghua@lexue.com>
2021-02-19 10:18:19 +0800

    创建项目

---
+++ LittlePink/LittlePink/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LittlePink
+//
+//  Created by qinxinghua on 2021/2/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LittlePink/LittlePink/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LittlePink
+//
+//  Created by qinxinghua on 2021/2/19.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
