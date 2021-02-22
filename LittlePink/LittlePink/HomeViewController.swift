fb09429ac0df2064dea5a3ac49fb1460ea09c8ee
qinxinghua <qinxinghua@lexue.com>
2021-02-22 09:40:04 +0800

    修改SB 绑定首页控制器

---
+++ LittlePink/LittlePink/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  LittlePink
+//
+//  Created by qinxinghua on 2021/2/19.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class HomeViewController: ButtonBarPagerTabStripViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
